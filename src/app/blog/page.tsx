9a56991b0fc4b79e5b1757cbaf639c0c35d4c4a1
julianthant <julianthant@gmail.com>
2025-07-05 22:42:35 -0700

    refactor: update project display and enhance layout structure

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,45 +1,49 @@
-import { FC } from 'react';
-import SkillTag from '@/components/SkillTag';
-import { dm_sans } from '@/library/fonts';
-import CodewarsDashboard from '@/components/CodewarsDashboard';
-import LeetcodeDashboard from '@/components/LeetcodeDashboard';
+import { FC } from "react";
+import Link from "next/link";
+import { dm_sans } from "@/library/fonts";
 
-import { HTML_Logo } from '@/components/svgs';
-import { CSS_Logo } from '@/components/svgs';
-import { JS_Logo } from '@/components/svgs';
-import { TS_Logo } from '@/components/svgs';
-import { SASS_Logo } from '@/components/svgs';
-import { React_Logo } from '@/components/svgs';
-import { Python_Logo } from '@/components/svgs';
-import { Tailwind_Logo } from '@/components/svgs';
-import { Astro_Logo } from '@/components/svgs';
-import { CPlusPlus_Logo } from '@/components/svgs';
-import { NextJS_Logo } from '@/components/svgs';
-import { NodeJS_Logo } from '@/components/svgs';
+import ProjectTag from "@/components/ProjectTag";
+import StyleZ from "@/assets/stylez-store.png";
+import ShoeStore from "@/assets/shoe-website.webp";
+import CodewarsDashboard from "@/components/CodewarsDashboard";
+import LeetcodeDashboard from "@/components/LeetcodeDashboard";
+
+import { HTML_Logo } from "@/components/svgs";
+import { CSS_Logo } from "@/components/svgs";
+import { JS_Logo } from "@/components/svgs";
+import { TS_Logo } from "@/components/svgs";
+import { SASS_Logo } from "@/components/svgs";
+import { React_Logo } from "@/components/svgs";
+import { Python_Logo } from "@/components/svgs";
+import { Tailwind_Logo } from "@/components/svgs";
+import { Astro_Logo } from "@/components/svgs";
+import { CPlusPlus_Logo } from "@/components/svgs";
+import { NextJS_Logo } from "@/components/svgs";
+import { NodeJS_Logo } from "@/components/svgs";
 
 interface pageProps {}
 
 const skills = [
-  { logo: HTML_Logo, lang: 'HTML', level: 'Advanced' },
-  { logo: CSS_Logo, lang: 'CSS', level: 'Intermediate' },
-  { logo: JS_Logo, lang: 'Javascript', level: 'Intermediate' },
-  { logo: TS_Logo, lang: 'Typescript', level: 'Intermediate' },
-  { logo: React_Logo, lang: 'React', level: 'Intermediate' },
-  { logo: NextJS_Logo, lang: 'Next JS', level: 'Intermediate' },
-  { logo: NodeJS_Logo, lang: 'Node JS', level: 'Intermediate' },
-  { logo: Python_Logo, lang: 'Python', level: 'Intermediate' },
-  { logo: Tailwind_Logo, lang: 'Tailwind', level: 'Intermediate' },
-  { logo: SASS_Logo, lang: 'SASS', level: 'Intermediate' },
-  { logo: Astro_Logo, lang: 'Astro', level: 'Intermediate' },
-  { logo: CPlusPlus_Logo, lang: 'C++', level: 'Beginner' },
+  { logo: HTML_Logo, lang: "HTML", level: "Advanced" },
+  { logo: CSS_Logo, lang: "CSS", level: "Intermediate" },
+  { logo: JS_Logo, lang: "Javascript", level: "Intermediate" },
+  { logo: TS_Logo, lang: "Typescript", level: "Intermediate" },
+  { logo: React_Logo, lang: "React", level: "Intermediate" },
+  { logo: NextJS_Logo, lang: "Next JS", level: "Intermediate" },
+  { logo: NodeJS_Logo, lang: "Node JS", level: "Intermediate" },
+  { logo: Python_Logo, lang: "Python", level: "Intermediate" },
+  { logo: Tailwind_Logo, lang: "Tailwind", level: "Intermediate" },
+  { logo: SASS_Logo, lang: "SASS", level: "Intermediate" },
+  { logo: Astro_Logo, lang: "Astro", level: "Intermediate" },
+  { logo: CPlusPlus_Logo, lang: "C++", level: "Beginner" },
 ];
 
 const page: FC<pageProps> = ({}) => {
   return (
-    <section className="min-h-screen pt-[83px] flex flex-col">
-      <div className="grid grid-flow-row flex-grow gap-8">
-        <div className="container px-8 flex flex-col justify-center gap-6">
-          <div className="text-center space-y-2">
+    <section className="flex flex-col pt-[100px] min-h-screen">
+      <div className="flex-grow gap-8 grid grid-flow-row">
+        <div className="flex flex-col justify-center gap-10 px-8 container">
+          <div className="space-y-2 text-center">
             <h2 className="font-bold text-4xl">Dashboard</h2>
             <h3
               className={`text-base text-gray-500 font-medium ${dm_sans.className}`}
@@ -47,31 +51,45 @@ const page: FC<pageProps> = ({}) => {
               Data Structures & Algorithms
             </h3>
           </div>
-          <div className="grid statsMD:grid-flow-col auto-cols-fr gap-12">
+          <div className="gap-12 grid statsMD:grid-flow-col auto-cols-fr">
             <CodewarsDashboard />
             <LeetcodeDashboard />
           </div>
         </div>
-        <div className="bg-accent-100 dark:bg-slate-900 gap-10 flex-grow justify-center flex flex-col py-8">
-          <div className="text-center space-y-2">
-            <h2 className="font-bold text-4xl">Learned Languages</h2>
+      </div>
+      <div className="flex-grow gap-8 grid grid-flow-row max-statsMD:pt-16">
+        <div className="flex flex-col justify-center gap-10 px-8 container">
+          <div className="space-y-2 text-center">
+            <h2 className="font-bold text-4xl">Featured Projects</h2>
             <h3
               className={`text-base text-gray-500 font-medium ${dm_sans.className}`}
             >
-              Frameworks & Libraries & Modules
+              Full-Stack Web Applications
             </h3>
           </div>
-          <div className="container px-8">
-            <ul role="list" className="grid-flow-container gap-x-12 gap-y-10">
-              {skills.map((skill, index) => (
-                <SkillTag
-                  key={index}
-                  SkillIcon={skill.logo}
-                  SkillLang={skill.lang}
-                  SkillLevel={skill.level}
-                />
-              ))}
-            </ul>
+          <div className="gap-12 grid statsMD:grid-flow-col auto-cols-fr">
+            <ProjectTag
+              ProjectImage={StyleZ}
+              Link="https://stylez.julianzaw.tech"
+              Name="Online Clothing Store"
+              Year="2024"
+              Tag="E-Commerce"
+            />
+            <ProjectTag
+              ProjectImage={ShoeStore}
+              Link="https://solesteals.netlify.app"
+              Name="Online Shoe Store"
+              Year="2023"
+              Tag="E-Commerce"
+            />
+          </div>
+
+          <div className="flex justify-end">
+            <Link href="/blog/projects">
+              <button className="hover:opacity-60 shadow-md px-5 py-2 rounded-lg font-bold text-white bg-accent-400">
+                View All Projects
+              </button>
+            </Link>
           </div>
         </div>
       </div>
