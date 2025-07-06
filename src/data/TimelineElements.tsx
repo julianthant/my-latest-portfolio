7f79898d27cc000f9f564e98ce3e43568583fe05
julianthant <julianthant@gmail.com>
2025-07-05 22:43:01 -0700

    refactor: standardize code formatting and improve readability in TimelineElements component

--- src/data/TimelineElements.tsx
+++ src/data/TimelineElements.tsx
@@ -1,48 +1,67 @@
-import MyPortfolioWebsite from '@/assets/my-portfolio.png';
-import CoffeeWebsite from '@/assets/coffee-website.png';
-import ToDoListWebsite from '@/assets/todolist-website.png';
-import CarShowroom from '@/assets/dad-showroom.png';
+import MyPortfolioWebsite from "@/assets/my-portfolio.png";
+import CoffeeWebsite from "@/assets/coffee-website.png";
+import ToDoListWebsite from "@/assets/todolist-website.png";
+import CarShowroom from "@/assets/dad-showroom.png";
+import SoleSteals from "@/assets/shoe-website.webp";
+import Yaps from "@/assets/yaps-website.webp";
+import StyleZ from "@/assets/stylez-store.png";
 
 let timelineElements = [
   {
     id: 1,
-    title: 'Portfolio Website',
-    url: 'https://julianhein.netlify.app',
-    description:
-      'The portfolio website I currently use. This website is built with Astro, React and SCSS which is more complex compared to the first portfolio website. The website structure and theme is similar, but the main difference is the frameworks. The coding dashboard is also linked with both codewars and leetcode apis to get the latest updates on my profile. The basic version was <a href="http://julianhein.great-site.net" target="_blank">http://julianhein.great-site.net</a>.',
-    date: '2023',
-    tag: 'Portfolio',
+    title: "Portfolio Website",
+    url: "https://julianhein.netlify.app",
+    year: "2023",
+    tag: "Portfolio",
     image: MyPortfolioWebsite,
   },
   {
     id: 2,
-    title: 'Coffee Website',
-    url: 'https://baristachawsu.netlify.app',
-    description:
-      'This is an improved version of the first coffee website I made for Barista Chaw Su. This improved version is built upon Astro, React and SCSS. The design is quite similar but the frameworks used are different. The basic one was <a href="http://baristachawsu.great-site.net" target="_blank">http://baristachawsu.great-site.net</a>.',
-    date: '2023',
-    tag: 'Educational',
+    title: "Coffee Website",
+    url: "https://baristachawsu.netlify.app",
+    year: "2023",
+    tag: "Educational",
     image: CoffeeWebsite,
   },
   {
     id: 3,
-    title: 'Car Showroom Website',
-    url: 'https:stylenation.netlify.app',
-    description:
-      "This is the website I made for my dad's business. He opens a car showroom so I made him a website for his showroom. The website shows the cars he sells and ways to contact him.",
-    date: '2023',
-    tag: 'Business',
+    title: "Car Showroom Website",
+    url: "https://stylenation.netlify.app",
+    year: "2023",
+    tag: "Business",
     image: CarShowroom,
   },
   {
     id: 4,
-    title: 'To Do List',
-    url: 'https://todolist-juju.netlify.app',
-    description:
-      'This is a todolist with advanced features such as login and signout. The user data is stored in a mongo db database and the items of the users are linked with the id of the user. This is made up of the MERN stack and the backend is hosted on Render. The basic version of the todolist can be found in <a href="https://juju-to-do-list.netlify.app" target="_blank">https://juju-to-do-list.netlify.app</a>.',
-    date: '2023',
-    tag: 'Utility',
+    title: "To Do List",
+    url: "https://todolist-juju.netlify.app",
+    year: "2023",
+    tag: "Utility",
     image: ToDoListWebsite,
+  },
+  {
+    id: 5,
+    title: "Online Shoe Store",
+    url: "https://solesteals.netlify.app",
+    year: "2023",
+    tag: "E-Commerce",
+    image: SoleSteals,
+  },
+  {
+    id: 6,
+    title: "Online Texting Website",
+    url: "https://yaps-social.vercel.app",
+    year: "2024",
+    tag: "Social",
+    image: Yaps,
+  },
+  {
+    id: 7,
+    title: "Online Clothing Store",
+    url: "https://stylez.julianzaw.tech",
+    year: "2024",
+    tag: "E-Commerce",
+    image: StyleZ,
   },
 ];
 
