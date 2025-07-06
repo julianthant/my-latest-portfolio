1765ea578ba8e8f965b638bebd4511f2fbfd1c01
julianthant <julianthant@gmail.com>
2025-07-05 22:42:51 -0700

    refactor: improve code formatting and enhance readability in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,34 @@
-import Link from 'next/link';
-import MobileNavigation from './MobileNavigation';
-import ToggleDayNight from './ToggleDayNight';
-import { dm_sans } from '@/library/fonts';
-import { Github_Logo } from './svgs';
+import Link from "next/link";
+import MobileNavigation from "./MobileNavigation";
+import ToggleDayNight from "./ToggleDayNight";
+import { dm_sans } from "@/library/fonts";
+import { Github_Logo } from "./svgs";
 
 export default function Navbar() {
   return (
-    <header className="fixed bg-white dark:bg-slate-900 py-3 inset-x-0 top-0 bottom-auto z-10 border-b-[1px] border-tr border-slate-200/20 drop-shadow-sm">
-      <div className="container px-8">
-        <div className="flex items-center justify-between">
+    <header className="top-0 bottom-auto z-10 fixed inset-x-0 bg-white dark:bg-slate-900 drop-shadow-sm py-3 border-slate-200/20 border-tr border-b-[1px]">
+      <div className="px-8 container">
+        <div className="flex justify-between items-center">
           <Link
             href="/"
             className={`font-bold text-3xl text-accent-400 ${dm_sans.className}`}
           >
-            julianhein
+            julianzaw
           </Link>
 
-          <div className="flex gap-6 items-center justify-center max-lg:hidden">
-            <nav className="border-r border-slate-200/20 pr-8">
+          <div className="max-lg:hidden flex justify-center items-center gap-6">
+            <nav className="pr-8 border-slate-200/20 border-r">
               <ul
                 role="list"
                 className="flex space-x-12 text-slate-600 dark:text-slate-400"
               >
-                <li className="hover:text-accent-350 font-bold text-base flex cursor-pointer">
+                <li className="flex font-bold text-base hover:text-accent-350 cursor-pointer">
                   <Link href="/">Home</Link>
                 </li>
-                <li className="hover:text-accent-350 font-bold text-base flex cursor-pointer">
+                <li className="flex font-bold text-base hover:text-accent-350 cursor-pointer">
                   <Link href="/blog/">About</Link>
                 </li>
-                <li className="hover:text-accent-350 font-bold text-base flex cursor-pointer">
-                  <Link href="/projects/">Projects</Link>
-                </li>
-                <li className="hover:text-accent-350 font-bold text-base flex cursor-pointer">
+                <li className="flex font-bold text-base hover:text-accent-350 cursor-pointer">
                   <Link href="/contact/">Contact Me</Link>
                 </li>
               </ul>
@@ -44,7 +41,7 @@ export default function Navbar() {
               target="_blank"
               rel="noreferrer"
             >
-              <Github_Logo className="hover:opacity-60 cursor-pointer fill-slate-500 dark:fill-slate-400 w-5 h-5" />
+              <Github_Logo className="hover:opacity-60 fill-slate-500 dark:fill-slate-400 w-5 h-5 cursor-pointer" />
             </a>
           </div>
 
